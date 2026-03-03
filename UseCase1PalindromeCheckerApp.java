35fdbfbf71849fb40fe2edd48562a235667bb482
burner <burnersmurner1000@gmail.com>
2026-03-03 15:54:59 +0530

    Use case 1

---
+++ UseCase1PalindromeCheckerApp.java
@@ -0,0 +1,34 @@
+/*IN CLASS - UseCase1PalindromeCheckerApp
+ *
+ *  * Use Case 1: Application Entry & Welcome Message
+ *  * Description:
+ *  * This class represents the entry point of the
+ *  * Palindrome Checker Management System.
+ *  * At this stage, the application:
+ *  * - Starts execution from the main() method
+ *  * - Displays a welcome message
+ *  * - Shows application version
+ **
+ *  * No palindrome logic is implemented yet.
+ *  *
+ *  * The goal is to establish a clear startup flow.
+ *  *
+ *  * @author Developer
+ *  * @version 1.0
+ *  */
+ public class UseCase1PalindromeCheckerApp {
+
+	     /**
+	      *      * Application entry point.
+	      *      *
+	      *      * This is the first method executed by the JVM
+	      *      * when the program starts.
+	      *      *
+	      *      * @param args Command-Line arguments
+	      *      */
+	     public static void main(String[] args) {
+		    System.out.println("Welcome to PalindromeChecker App");
+		    System.out.println("Version: 1.0.0");
+		    }
+ }
+
